cad458c15d512515f6be50145594d7dcbc4e3e8b
Anushka Mali <anushkamali@Anushkas-MacBook-Air.local>
2023-02-27 14:51:37 +0530

    conversion of money

--- code.js
+++ code.js
@@ -28,13 +28,21 @@ export class Portfolio {
         this.moneys = [];
     }
 
+    convert(money, currency) {
+        let eurToUsd = 1.2; 1
+        if (money.currency === currency) {
+            return money.amount;
+        }
+        return money.amount * eurToUsd; 2
+    }
+
     add(...moneys) {
         this.moneys = this.moneys.concat(moneys);
     }
 
     evaluate(currency) {
         let total = this.moneys.reduce( (sum, money) => {
-            return sum + money.amount;
+            return sum + this.convert(money, currency);
         }, 0);
         return new Money(total, currency);
     }
--- tests.js
+++ tests.js
@@ -1,36 +1,50 @@
 import assert from 'assert';
-import { Dollar, Money, Portfolio } from './code.js';
+import { Money, Portfolio } from './code.js';
 
-//1
-let fiver = new Dollar(5);
+let fiver = new Money(5, "USD");
 let tenner = fiver.times(2);
 assert.strictEqual(tenner.amount, 10);
+assert.strictEqual(tenner.currency, "USD");
 
-//2
-let tenEuros = new Money(10, "EUR");
-let tewntyEuros = tenEuros.times(2);
-assert.strictEqual(tewntyEuros.amount, 20);
-assert.strictEqual(tewntyEuros.currency, "EUR");
+class MoneyTest {
+    testMultiplication() {
+        let tenEuros = new Money(10, "EUR");
+        let tewntyEuros = tenEuros.times(2);
+        assert.strictEqual(tewntyEuros.amount, 20);
+        assert.strictEqual(tewntyEuros.currency, "EUR");
+    }
 
-//3
-let fiver1 = new Money(5, "USD");
-let tenner1 = fiver1.times(2);
-assert.strictEqual(tenner1.amount, 10);
-assert.strictEqual(tenner1.currency, "USD");
+    testDivision() {
+        let originalMoney = new Money(4002, "KRW");
+        let actualMoneyAfterDivision = originalMoney.divide(4);
+        let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+        assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+    }
 
-//4
-let originalMoney = new Money(4002, "KRW");
-let actualMoneyAfterDivision = originalMoney.divide(4);
-let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
-assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+    testAddition() {
+        let fiveDollars = new Money(5, "USD");
+        let tenDollars = new Money(10, "USD");
+        let fifteenDollars = new Money(15, "USD");
+        let portfolio = new Portfolio(); 
+        portfolio.add(fiveDollars, tenDollars); 
+        assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+    }
 
-//5
-let fiveDollars = new Money(5, "USD");
-let tenDollars = new Money(10, "USD");
-assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
+    testAdditionOfDollarsAndEuros() {
+        let fiveDollars = new Money(5, "USD");
+        let tenEuros = new Money(10, "EUR");
+        let portfolio = new Portfolio(); 
+        portfolio.add(fiveDollars, tenEuros); 
+        let expectedValue = new Money(17, "USD");
+        assert.deepStrictEqual(portfolio.evaluate("USD"), expectedValue);
+    }
 
-//6
-let fifteenDollars = new Money(15, "USD");
-let portfolio = new Portfolio(); 
-portfolio.add(fiveDollars, tenDollars); 
-assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
+    runAllTests() {
+        this.testAddition();
+        this.testDivision();
+        this.testMultiplication();
+        this.testAdditionOfDollarsAndEuros();
+    }
+}
+
+new MoneyTest().runAllTests();
